7f84b0e9d7a8799605f764a30f5891781120046e
javier-bustillo <jjavier.bustillo@gmail.com>
2022-09-18 17:04:34 +0000

    dropleft w/ item bin added

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,69 +1,61 @@
 import React from "react";
-import starWarsLogo from "../../img/star-wars-logo-1.png";
 import { Link } from "react-router-dom";
-import "../../styles/navbar.css";
+
+import starWarsLogo from "../../img/Temp-150x91.jpg";
+import { ImBin } from "react-icons/im";
+class Bin extends React.Component {
+  render() {
+    <ImBin />;
+  }
+}
+/* import "../../styles/navbar.css"; */
 
 export const Navbar = () => {
   return (
     <>
-      <nav className="navbar navbar-expand-lg">
+      <nav className="navbar navbar-expand-lg navbar-light bg-light">
         <div className="container-fluid">
           <Link to="/">
             <a className="navbar-brand">
               <img className="ms-5" src={starWarsLogo} alt="star wars logo" />
             </a>
           </Link>
-          <div className="me-5">
+          <div className="btn-group dropstart me-5">
             <button
-              className="navbar-toggler"
               type="button"
-              data-bs-toggle="collapse"
-              data-bs-target="#navbarNavDarkDropdown"
-              aria-controls="navbarNavDarkDropdown "
+              className="btn btn-dark dropdown-toggle"
+              data-bs-toggle="dropdown"
               aria-expanded="false"
-              aria-label="Toggle navigation"
             >
-              <span className="navbar-toggler-icon"></span>
+              &nbsp;<span className="badge bg-danger rounded-circle">4</span>
+              &nbsp;Read Later
             </button>
-            <div
-              className="collapse navbar-collapse"
-              id="navbarNavDarkDropdown"
-            >
-              <ul className="navbar-nav">
-                <li className="nav-item dropdown">
-                  <a
-                    className="nav-link dropdown-toggle"
-                    href="#"
-                    id="navbarDarkDropdownMenuLink"
-                    role="button"
-                    data-bs-toggle="dropdown"
-                    aria-expanded="false"
-                  >
-                    Read Later
-                  </a>
-                  <ul
-                    className="dropdown-menu dropdown-menu-dark"
-                    aria-labelledby="navbarDarkDropdownMenuLink"
-                  >
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Action
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Another action
-                      </a>
-                    </li>
-                    <li>
-                      <a className="dropdown-item" href="#">
-                        Something else here
-                      </a>
-                    </li>
-                  </ul>
-                </li>
-              </ul>
-            </div>
+            <ul className="dropdown-menu">
+              <li>
+                <a className="dropdown-item" href="#">
+                  Menu item &nbsp;&nbsp;
+                  <button className="border-0 bg-transparent">
+                    <ImBin />
+                  </button>
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Menu item &nbsp;&nbsp;
+                  <button className="border-0 bg-transparent">
+                    <ImBin />
+                  </button>
+                </a>
+              </li>
+              <li>
+                <a className="dropdown-item" href="#">
+                  Menu item &nbsp;&nbsp;
+                  <button className="border-0 bg-transparent">
+                    <ImBin />
+                  </button>
+                </a>
+              </li>
+            </ul>
           </div>
         </div>
       </nav>
--- src/js/views/home.js
+++ src/js/views/home.js
@@ -3,13 +3,16 @@ import rigoImage from "../../img/rigo-baby.jpg";
 import "../../styles/home.css";
 
 export const Home = () => (
-	<div className="text-center mt-5">
-		<h1>Hello Rigo!</h1>
-		<p>
-			<img src={rigoImage} />
-		</p>
-		<a href="#" className="btn btn-success">
-			If you see this green button, bootstrap is working
-		</a>
-	</div>
+  <div className="mt-5 ms-5">
+    <div className="ms-3">
+      <h1>Characters</h1>
+    </div>
+
+    <p>
+      <img src={rigoImage} />
+    </p>
+    <a href="#" className="btn btn-success">
+      If you see this green button, bootstrap is working
+    </a>
+  </div>
 );
