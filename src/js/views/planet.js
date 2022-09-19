24b704011f9619745a5e774c3dcbdc1dd08b61d6
javier-bustillo <jjavier.bustillo@gmail.com>
2022-09-19 18:32:58 +0000

    planet view added

--- src/js/views/planet.js
+++ src/js/views/planet.js
@@ -4,28 +4,26 @@ import starWarsPlanet from "../../img/planet-sample.png";
 import "../../styles/home.css";
 
 export const Planet = () => (
-  <div className="d-flex justify-content-center m-5">
-    <div className="card mb-3">
-      <div className="row g-0">
-        <div className="col-md-4">
-          <img
-            src={starWarsPlanet}
-            className="img-fluid rounded-start"
-            alt="..."
-          />
-        </div>
-        <div className="col-md-8">
-          <div className="card-body">
-            <h5 className="card-title">Card title</h5>
-            <p className="card-text">
-              This is a wider card with supporting text below as a natural
-              lead-in to additional content. This content is a little bit
-              longer.
-            </p>
-            <p className="card-text">
-              <small className="text-muted">Last updated 3 mins ago</small>
-            </p>
-          </div>
+  <div className="d-flex row justify-content-center m-5">
+    <div className="card mb-3 border-0" style={{ maxWidth: "1500px" }}>
+      <div className="col-md-4">
+        <img
+          src={starWarsPlanet}
+          className="img-fluid rounded-start mt-5"
+          alt="..."
+        />
+      </div>
+
+      <div className="col-md-3 mt-5 text-end">
+        <div className="card-body">
+          <h5 className="card-title">Card title</h5>
+          <p className="card-text">
+            This is a wider card with supporting text below as a natural lead-in
+            to additional content. This content is a little bit longer.
+          </p>
+          <p className="card-text">
+            <small className="text-muted">Last updated 3 mins ago</small>
+          </p>
         </div>
       </div>
     </div>
