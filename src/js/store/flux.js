7f375a9f67694f9d5b0de2982775475b38c5d79c
javier-bustillo <jjavier.bustillo@gmail.com>
2022-12-08 17:03:44 +0000

    fetch to get vehicles

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,92 +1,94 @@
-const getState = ({ getStore, getActions, setStore }) => {
-  return {
-    store: {
-      demo: [
-        {
-          title: "FIRST",
-          background: "white",
-          initial: "white",
+const getState = ({
+    getStore,
+    getActions,
+    setStore
+}) => {
+    return {
+        store: {
+            characters: [],
+            planets: [],
+            readMoreList: [],
         },
-        {
-          title: "SECOND",
-          background: "white",
-          initial: "white",
+        actions: {
+            // Use getActions to call a function within a fuction
+            exampleFunction: () => {
+                getActions().changeColor(0, "green");
+            },
+            loadSomeData: () => {
+                /**
+                            fetch().then().then(data => setStore({ "foo": data.bar }))
+                        */
+            },
+            getCharacter: () => {
+                fetch("https://swapi.dev/api/people")
+                    .then((response) => response.json())
+                    .then((data) =>
+                        setStore({
+                            characters: data.results,
+                        })
+                    )
+                    .catch((err) => console.error(err));
+            },
+            getPlanet: () => {
+                fetch("https://swapi.dev/api/planets")
+                    .then((response) => response.json())
+                    .then((data) =>
+                        setStore({
+                            planets: data.results,
+                        })
+                    )
+                    .catch((err) => console.error(err));
+            },
+            getVehicles: () => {
+                fetch("https://swapi.dev/api/vehicles")
+                    .then((response) => response.json())
+                    .then((data) =>
+                        setStore({
+                            vehicles: data.results,
+                        })
+                    )
+                    .catch((err) => console.error(err));
+            },
+            addReadMoreItem: (item) => {
+                const store = getStore();
+                if (!store.readMoreList.includes(item)) {
+                    setStore({
+                        ...store.readMoreList,
+                        readMoreList: store.readMoreList.concat(item),
+                    });
+                }
+            },
+            delReadMoreItem: (whateverItem) => {
+                const store = getStore();
+                const itemListed = store.readMoreList;
+
+                let readMoreListFiltered = itemListed.filter(
+                    (item) => item !== whateverItem
+                );
+
+                setStore({
+                    readMoreList: readMoreListFiltered,
+                });
+            },
+
+            changeColor: (index, color) => {
+                //get the store
+                const store = getStore();
+
+                //we have to loop the entire demo array to look for the respective index
+                //and change its color
+                const demo = store.demo.map((elm, i) => {
+                    if (i === index) elm.background = color;
+                    return elm;
+                });
+
+                //reset the global store
+                setStore({
+                    demo: demo,
+                });
+            },
         },
-      ],
-      characters: [],
-      planets: [],
-      readMoreList: [],
-    },
-    actions: {
-      // Use getActions to call a function within a fuction
-      exampleFunction: () => {
-        getActions().changeColor(0, "green");
-      },
-      loadSomeData: () => {
-        /**
-                    fetch().then().then(data => setStore({ "foo": data.bar }))
-                */
-      },
-      getCharacter: () => {
-        fetch("https://swapi.dev/api/people")
-          .then((response) => response.json())
-          .then((data) =>
-            setStore({
-              characters: data.results,
-            })
-          )
-          .catch((err) => console.error(err));
-      },
-      getPlanet: () => {
-        fetch("https://swapi.dev/api/planets")
-          .then((response) => response.json())
-          .then((data) =>
-            setStore({
-              planets: data.results,
-            })
-          )
-          .catch((err) => console.error(err));
-      },
-      addReadMoreItem: (item) => {
-        const store = getStore();
-        if (!store.readMoreList.includes(item)) {
-          setStore({
-            ...store.readMoreList,
-            readMoreList: store.readMoreList.concat(item),
-          });
-        }
-      },
-      delReadMoreItem: (whateverItem) => {
-        const store = getStore();
-        const itemListed = store.readMoreList;
-
-        let readMoreListFiltered = itemListed.filter(
-          (item) => item !== whateverItem
-        );
-
-        setStore({
-          readMoreList: readMoreListFiltered,
-        });
-      },
-
-      changeColor: (index, color) => {
-        //get the store
-        const store = getStore();
-
-        //we have to loop the entire demo array to look for the respective index
-        //and change its color
-        const demo = store.demo.map((elm, i) => {
-          if (i === index) elm.background = color;
-          return elm;
-        });
-
-        //reset the global store
-        setStore({
-          demo: demo,
-        });
-      },
-    },
-  };
+    };
 };
 
 export default getState;
